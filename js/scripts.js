43747fc9072649b1a331c6f0fe376c686e0b2b31
April Soetarman <aprilsoetarman@gmail.com>
2021-02-12 20:30:28 -0800

    add submit form and front end logic

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Business logic (backend)
+
+
+
+
+
+// User Interface logic (front end)
+
+$(document).ready(function()
+  $("form#numReplace").submit(function(event) {
+    event.preventDefault();
+
+
+
+    $("output").text(result);
+  });
+  
+});
+
