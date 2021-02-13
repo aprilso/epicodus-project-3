a58b7418af565026bb38183a66f7b86b66cfca4b
April Soetarman <aprilsoetarman@gmail.com>
2021-02-12 22:32:32 -0800

    remove parseInt so it can return a string to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 // Business logic (backend)
 
-function beepBoop(number1) {
-  return number1 + 5;
+function beepBoop(number) {
+  // for (var i=0, i<number; i++) {
+  //   if (i == 1)
+  //     return "Beep!";
+  //   else if (i == 2)
+  //     return "Boop!"
+  //   else if (i == 3)
+  //     return "Won't you be my neighbor?"
+  //   else
+  //     return i;
+  // }
+  return "Beep!" + number;
 }
+
+
+
 
 
 // User Interface logic (front end)
@@ -10,9 +23,8 @@ function beepBoop(number1) {
 $(document).ready(function() {
   $("form#submission").submit(function(event) {
     event.preventDefault();
-  
-    const number1 = parseInt($("#numInput").val());
-    const result = beepBoop(number1);
+    const number = $("#numInput").val();
+    const result = beepBoop(number);
     $("#output").text(result);
   });
   
