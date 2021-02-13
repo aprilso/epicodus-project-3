17e906021784c34e63ff81c57cb6e5ebf922e0d3
April Soetarman <aprilsoetarman@gmail.com>
2021-02-12 21:10:25 -0800

    add front end logic (displays an output)

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 // Business logic (backend)
 
-
-
+function beepBoop(number1) {
+  return number1 + 5;
+}
 
 
 // User Interface logic (front end)
 
-$(document).ready(function()
-  $("form#numReplace").submit(function(event) {
+$(document).ready(function() {
+  $("form#submission").submit(function(event) {
     event.preventDefault();
-
-
-
-    $("output").text(result);
+  
+    const number1 = parseInt($("#numInput").val());
+    const result = beepBoop(number1);
+    $("#output").text(result);
   });
   
 });
